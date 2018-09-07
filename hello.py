f7529ebcdb28bc486cc11dcfdd8a7fa2a6eb2d31
carlosdanielcsantos <carlosdanielcsantos@gmail.com>
2018-09-07 12:27:27 +0100

    Fix bug (#1)
    * Fix bug when no argument is passed
    
    * Rename hello_world

---
+++ hello.py
@@ -0,0 +1,5 @@
+import sys
+if len(sys.argv[1:]) == 1:
+    print("Hello, {}!".format(sys.argv[1]))
+else:
+    print("Hello, World!")
--- hello_world.py
+++
@@ -1,2 +0,0 @@
-import sys
-print("Hello, {}!".format(sys.argv[1]))
